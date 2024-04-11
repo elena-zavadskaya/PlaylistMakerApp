4a8903e1e59c9f5819d54df260004c7c5c72e45e
elena-zavadskaya <ele.zavadskaya.na@gmail.com>
2024-04-11 20:22:47 +0300

    Спринт 14.

--- app/src/main/java/com/practicum/playlistmakerapp/AudioPlayerActivity.kt
+++ app/src/main/java/com/practicum/playlistmakerapp/AudioPlayerActivity.kt
@@ -1,28 +1,48 @@
 package com.practicum.playlistmakerapp
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Adapter
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.google.gson.Gson
-import com.practicum.playlistmakerapp.databinding.ActivitySearchBinding
 import com.practicum.playlistmakerapp.databinding.AudioPlayerBinding
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class AudioPlayerActivity : AppCompatActivity() {
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+
+        private const val DELAY = 1000L
+    }
+
+    private var playerState = STATE_DEFAULT
+    private var mediaPlayer = MediaPlayer()
+    private lateinit var play: ImageView
+    private lateinit var pause: ImageView
 
     private lateinit var binding: AudioPlayerBinding
     private lateinit var chosenTrack: Track
+
+    private var mainThreadHandler: Handler? = null
+    private var duration: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = AudioPlayerBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        mainThreadHandler = Handler(Looper.getMainLooper())
 
         binding.backButton.setOnClickListener {
             finish()
@@ -40,6 +60,10 @@ class AudioPlayerActivity : AppCompatActivity() {
         val releaseDate: TextView = findViewById(R.id.releaseDateTV)
         val primaryGenreName: TextView = findViewById(R.id.primaryGenreNameTV)
         val country: TextView = findViewById(R.id.countryTV)
+        play = findViewById(R.id.playIV)
+        pause = findViewById(R.id.pauseIV)
+        duration = findViewById(R.id.durationTV)
+
 
         trackName.text = chosenTrack.trackName
 
@@ -56,8 +80,84 @@ class AudioPlayerActivity : AppCompatActivity() {
 
         collectionName.text = chosenTrack.collectionName
         releaseDate.text = chosenTrack.releaseDate.substring(0,4)
-
         primaryGenreName.text = chosenTrack.primaryGenreName
         country.text = chosenTrack.country
+
+        preparePlayer(chosenTrack.previewUrl)
+
+        play.setOnClickListener {
+            startTimer()
+            playbackControl()
+        }
+
+        pause.setOnClickListener {
+            mainThreadHandler?.removeCallbacksAndMessages(null)
+            playbackControl()
+        }
+    }
+
+    private fun startTimer() {
+        mainThreadHandler?.post(
+            timeCount()
+        )
+    }
+
+    private fun timeCount(): Runnable {
+        return object : Runnable {
+            override fun run() {
+                val seconds = mediaPlayer.currentPosition / DELAY
+                duration?.text = String.format("%d:%02d", seconds / 60, seconds % 60)
+                mainThreadHandler?.postDelayed(this, DELAY).toString()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+    private fun playbackControl() {
+        when(playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    private fun preparePlayer(url: String) {
+        mediaPlayer.setDataSource(url)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            playerState = STATE_PREPARED
+            play.visibility = View.VISIBLE
+            pause.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        playerState = STATE_PLAYING
+        play.visibility = View.INVISIBLE
+        pause.visibility = View.VISIBLE
+
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playerState = STATE_PAUSED
+        play.visibility = View.VISIBLE
+        pause.visibility = View.INVISIBLE
     }
 }
--- app/src/main/java/com/practicum/playlistmakerapp/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmakerapp/SearchActivity.kt
@@ -5,8 +5,11 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -22,21 +25,21 @@ class SearchActivity : AppCompatActivity() {
         private const val SHARED_PREFERENCES_FOR_SEARCH_HISTORY = "shared_preference_for_search_history"
         const val SEARCH_HISTORY_KEY = "key_for_dark_theme"
         private const val KEY = "KEY"
+        private const val SEARCH_DEBOUNCE_DELAY = 2000L
     }
 
     private lateinit var binding: ActivitySearchBinding
-
     private lateinit var searchValue: String
-
     private val trackBaseUrl = "https://itunes.apple.com"
     private val retrofit = Retrofit.Builder()
         .baseUrl(trackBaseUrl)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
     private val searchService = retrofit.create(ITunesApi::class.java)
-
     private val tracks: MutableList<Track> = mutableListOf()
     private lateinit var trackAdapter: TrackAdapter
+    private val searchRunnable = Runnable { search() }
+    private val handler = Handler(Looper.getMainLooper())
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -102,6 +105,11 @@ class SearchActivity : AppCompatActivity() {
             }
         }
 
+        fun searchDebounce() {
+            handler.removeCallbacks(searchRunnable)
+            handler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
+        }
+
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
@@ -116,6 +124,8 @@ class SearchActivity : AppCompatActivity() {
                         searchHistoryAdapter.notifyDataSetChanged()
                         showSearchHistory()
                     }
+                } else if (p0?.isEmpty() == false){
+                    searchDebounce()
                 }
             }
 
@@ -154,7 +164,6 @@ class SearchActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         searchValue = binding.inputEditText.text.toString()
-        // ???
         outState.putString(KEY, searchValue)
     }
 
@@ -166,6 +175,11 @@ class SearchActivity : AppCompatActivity() {
 
     private fun search() {
         if (searchValue.isNotEmpty()) {
+            binding.searchHistory.visibility = View.GONE
+            binding.recyclerView.visibility = View.GONE
+            binding.notFound.visibility = View.GONE
+            binding.internetError.visibility = View.GONE
+            binding.progressBar.visibility = View.VISIBLE
             searchService
                 .trackSearch(searchValue)
                 .enqueue(object : Callback<TrackResponse> {
@@ -173,10 +187,12 @@ class SearchActivity : AppCompatActivity() {
                         call: Call<TrackResponse>,
                         response: Response<TrackResponse>
                     ) {
+                        binding.progressBar.visibility = View.GONE
                         checkResponse(response)
                     }
 
                     override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                        binding.progressBar.visibility = View.GONE
                         showInternetErrorPage()
                     }
                 })
